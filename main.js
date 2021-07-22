6b877e76d579b28d1833bdd291f573f6ab02f79e
VihaanPro <76435114+VihaanPro@users.noreply.github.com>
2021-07-22 16:14:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+object=[];
+video="";
+function preload(){
+    video=createVideo("video.mp4");
+    video.hide();
+}
+function setup(){
+    canvas=createCanvas(480,380);
+    canvas.center();
+}
+function draw(){
+    image(video,0,0,480,380);
+    if(status!=""){
+        objectDetector.detect(video,gotReslt);
+    for(i=0; i< object.length; i++){
+        document.getElementById("status").innerHTML="Status : Objects Detected";
+        document.getElementById("number_of_objects").innerHTML="Number of objects detected are :"+ object.length;
+        fill("#FF0000");
+        percent=floor(object[i].confidence*100);
+        text(object[i].label + "  " + percent +"%", object[i].x+15, object[i].y+15);
+        noFill();
+    stroke("#FF0000");
+    rect(object[i].x,object[i].y,object[i].width, object[i].height);
+    
+    var synth = window.speechSynthesis;
+          speak_data = object[i].label;
+          var utterThis = new SpeechSynthesisUtterance;
+          synth.speak(utterThis);
+    }
+    }
+}
+function start(){
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status: Detecting Objects";
+}
+function modelLoaded(){
+    console.log("model Loaded!");
+    status=true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
+function gotReslt(error,results){
+if(error){
+    console.log(error);
+}
+console.log(results);
+object=results;
+}
